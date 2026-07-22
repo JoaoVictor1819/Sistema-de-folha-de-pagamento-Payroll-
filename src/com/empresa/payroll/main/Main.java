b0b055e43b1ad718dc404326541f206afb38e9a2
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-22 11:29:13 -0300

    Conclusao de dados na classe FreelancePJ e adicionado o objeto no main para ser exibido no terminal

--- src/com/empresa/payroll/main/Main.java
+++ src/com/empresa/payroll/main/Main.java
@@ -2,6 +2,7 @@ package com.empresa.payroll.main;
 
 import com.empresa.payroll.enums.TipoContratos;
 import com.empresa.payroll.model.Clt;
+import com.empresa.payroll.model.FreelancePJ;
 import com.empresa.payroll.model.Funcionario;
 import com.empresa.payroll.model.Gerente;
 
@@ -14,6 +15,9 @@ public class Main {
         Clt clt = new Clt("Giovana", "10/12/2022", 123143234, 2400);
         System.out.println(clt.toString());
         System.out.println(clt.gerarContrato());
-
+        System.out.println("========================");
+        FreelancePJ freelancePJ = new FreelancePJ("Juliana", "21/05/2026", 123123312, 80, 6, "21/05/2026", "Adaptar sistema de empresa a um cliente expecifica", "12/06/2026");
+        System.out.println(freelancePJ.toString());
+        System.out.println(freelancePJ.gerarContrato());
     }
 }
--- src/com/empresa/payroll/model/FreelancePJ.java
+++ src/com/empresa/payroll/model/FreelancePJ.java
@@ -1,4 +1,50 @@
 package com.empresa.payroll.model;
 
-public class FreelancePJ {
+import com.empresa.payroll.contratos.Contratavel;
+import com.empresa.payroll.enums.TipoContratos;
+
+public class FreelancePJ extends Funcionario implements Contratavel {
+    private int horasTrabalhadas;
+    private String dataInicioContrato;
+    private String datafimContrato;
+    private String objetivoDoTrabalho;
+
+    protected TipoContratos tipoContratos = TipoContratos.PJ;
+
+    public FreelancePJ(String nome, String dataAdmissao, int documento, double salario, int horasTrabalhadas, String dataInicioContrato, String objetivoDoTrabalho, String datafimContrato) {
+        super(nome, dataAdmissao, documento, salario);
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.dataInicioContrato = dataInicioContrato;
+        this.datafimContrato = datafimContrato;
+        this.objetivoDoTrabalho = objetivoDoTrabalho;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return this.salario * horasTrabalhadas;
+    }
+
+    @Override
+    public String gerarContrato() {
+        return this.nome +
+                " Contratado(a) como " +tipoContratos.name()+
+                ", Com o objetivo de "+ this.objetivoDoTrabalho+
+                ", data incial do contrato "+this.dataInicioContrato+
+                ", data fim do contrato "+this.datafimContrato+
+                ", Remuneracao por horas trabalhadas {"+this.salario+"R$ por horas}"+
+                ", Total ao fim do contrato = "+ this.horasTrabalhadas +"hrs Trabalhadas | Total = "+calcularSalario();
+    }
+
+    @Override
+    public String toString() {
+        return "FreelancePJ{" +
+                "nome='" + nome + '\'' +
+                ", horasTrabalhadas=" + horasTrabalhadas +
+                ", dataInicioContrato='" + dataInicioContrato + '\'' +
+                ", datafimContrato='" + datafimContrato + '\'' +
+                ", objetivoDoTrabalho='" + objetivoDoTrabalho + '\'' +
+                ", tipoContratos=" + tipoContratos +
+                ", documento=" + documento +
+                ", salario=" + salario + "R$}";
+    }
 }
