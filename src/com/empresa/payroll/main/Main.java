f7834d8ae992e4d6e4de871dfec3b9723f1f584d
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-22 14:18:26 -0300

    finalizado model

--- src/com/empresa/payroll/main/Main.java
+++ src/com/empresa/payroll/main/Main.java
@@ -1,10 +1,10 @@
 package com.empresa.payroll.main;
 
 import com.empresa.payroll.enums.TipoContratos;
-import com.empresa.payroll.model.Clt;
-import com.empresa.payroll.model.FreelancePJ;
-import com.empresa.payroll.model.Funcionario;
-import com.empresa.payroll.model.Gerente;
+import com.empresa.payroll.model.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
@@ -19,5 +19,9 @@ public class Main {
         FreelancePJ freelancePJ = new FreelancePJ("Juliana", "21/05/2026", 123123312, 80, 6, "21/05/2026", "Adaptar sistema de empresa a um cliente expecifica", "12/06/2026");
         System.out.println(freelancePJ.toString());
         System.out.println(freelancePJ.gerarContrato());
+        System.out.println("=============================");
+        Estagiario estagiario = new Estagiario("Victor", "05/03/2027", 1123424423, 0, 860);
+        System.out.println(estagiario.toString());
+        System.out.println(estagiario.gerarContrato());
     }
 }
--- src/com/empresa/payroll/model/Estagiario.java
+++ src/com/empresa/payroll/model/Estagiario.java
@@ -1,4 +1,42 @@
 package com.empresa.payroll.model;
 
-public class Estagiario {
+
+import com.empresa.payroll.contratos.Contratavel;
+import com.empresa.payroll.enums.TipoContratos;
+
+public class Estagiario extends Funcionario implements Contratavel {
+    public static final double AUXILIOTRANSPORTE = 100;
+    private double bolsaFixa;
+
+    protected TipoContratos tipoContratos = TipoContratos.ESTAGIARIO;
+
+    public Estagiario(String nome, String dataAdmissao, int documento, double salario, double bolsaFixa) {
+        super(nome, dataAdmissao, documento, salario);
+        this.bolsaFixa = bolsaFixa;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return bolsaFixa + AUXILIOTRANSPORTE;
+    }
+
+    @Override
+    public String gerarContrato() {
+        return this.nome+
+                " contratado(a) como "+tipoContratos.name()+
+                ", possui um bolsa fixa de "+this.bolsaFixa+
+                " mais auxilio transporte de "+AUXILIOTRANSPORTE+ " Que no total = "+calcularSalario()+"R$";
+    }
+
+    @Override
+    public String toString() {
+        return "Estagiario{" +
+                "nome='" + nome + '\'' +
+                ", documento=" + documento + '\'' +
+                ", tipoContratos=" + tipoContratos +
+                ", dataAdmissao='" + dataAdmissao + '\'' +
+                ", bolsa fixa="+ bolsaFixa + '\'' +
+                ", auxilio transporte="+ AUXILIOTRANSPORTE + '\'' +
+                ", total= " + calcularSalario()+"R$"+'}';
+    }
 }
